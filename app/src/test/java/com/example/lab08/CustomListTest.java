012b9db402e4af2b11042582fed29e6cc4b07c3f
Gurbnova <gurbanov@ualberta.ca>
2026-03-05 20:16:41 -0700

    adding a failing test (deleteCity)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,12 +3,24 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
-        @Test
-        public void testHasCity() {
-            CustomList list = new CustomList();
-            City calgary = new City("Calgary", "AB");
-            list.addCity(calgary);
-            // This will fail initially because hasCity() doesn't exist
-            assertTrue(list.hasCity(calgary));
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        // This will fail initially because hasCity() doesn't exist
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        list.addCity(calgary);
+
+        list.deleteCity(calgary);
+
+        assertFalse(list.hasCity(calgary));
     }
 }
